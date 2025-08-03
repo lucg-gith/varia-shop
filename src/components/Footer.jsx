aeaba0a2165dcd367cab0f22f8861dba68c765da
lucg-gith <garrelucia@gmail.com>
2025-08-03 21:15:34 +0100

    add: Footer content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,22 @@
 const Footer = () => {
-  return <p>This is the Footer</p>;
+  return (
+    <footer>
+      <p>
+        Proyecto creado por{" "}
+        <a
+          href="https://www.linkedin.com/in/luciagarre/?originalSubdomain=uk"
+          target="_blank"
+        >
+          {" "}
+          Lucia Garre
+        </a>{" "}
+      </p>
+      <p>
+        para el trabajo final del Curso Programador Web Inicial - Front End
+        Developer - UTN - 2025
+      </p>
+    </footer>
+  );
 };
 
 export { Footer };
