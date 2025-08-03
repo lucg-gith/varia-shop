a9f13817d577f3fdeca4ea5b0f950923dfdfcde5
lucg-gith <garrelucia@gmail.com>
2025-08-04 00:07:59 +0100

    add: Not Found page content

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,15 @@ import { Layout } from "../components/Layout";
 const NotFound = () => {
   return (
     <Layout>
-      <p>This is the NotFound</p>;
+      <h1>Página no encontrada</h1>
+      <p>
+        Lo sentimos, la página que estás buscando no existe. Es posible que la
+        dirección haya sido escrita incorrectamente o que la página haya sido
+        movida.
+      </p>
+      <p>
+        Puedes volver a la <Link to="/">página de inicio</Link>
+      </p>
     </Layout>
   );
 };
