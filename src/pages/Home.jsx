e2b4db5dc6aaa131cb17c695ad4f2662ae9cc92d
lucg-gith <garrelucia@gmail.com>
2025-08-04 19:16:03 +0100

    feat: fetch functionality - error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,24 @@
 import { Layout } from "../components/Layout";
 import { useEffect } from "react";
+import { useState } from "react";
 
 const Home = () => {
-  //NOTA: fetch herramienta nativa para hacer peticiones a la api - por defecto usa GET
+  //#region
+  // NOTA: fetch herramienta nativa para hacer peticiones a la api - por defecto usa GET
   // siempre que hay una funcion asincrona hay un await
   // decirle a react que muestre el return y despues cargue los productos - con effecto secundario - useEffect
+  //#endregion
+  const [products, setProduct] = useState([]);
 
   const fetchingProducts = async () => {
     const response = await fetch("https://fakestoreapi.com/products", {
       method: "GET",
     });
     const data = await response.json();
-    console.log(data);
+    setProduct(data);
   };
 
-  // NOTA: syntaxtis useEffect(() => { aca va lo que se carga de forma secundaria}, [cuantas veces se ejecucuta, si esta vacio se ejecuta una sola vez])
+  // NOTA: syntaxis useEffect(() => { aca va lo que se carga de forma secundaria}, [cuantas veces se ejecucuta, si esta vacio se ejecuta una sola vez])
 
   useEffect(() => {
     fetchingProducts();
@@ -23,6 +27,10 @@ const Home = () => {
   return (
     <Layout>
       <p>This is the Home</p>
+      <p>testing</p>
+      {products.map((product) => (
+        <h2>{product.title}</h2>
+      ))}
     </Layout>
   );
 };
