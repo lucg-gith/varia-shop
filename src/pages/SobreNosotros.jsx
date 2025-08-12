7f02d23c2a53b5f830b2ea35dbd7729ba2c18944
lucg-gith <garrelucia@gmail.com>
2025-08-12 20:14:44 +0100

    add: static content SobreNosotros page

---
+++ src/pages/SobreNosotros.jsx
@@ -0,0 +1,36 @@
+const SobreNosotros = () => {
+  return (
+    <div>
+      <h1>Sobre Nosotros</h1>
+      <section>
+        <h2>El proyecto </h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi et
+          dolores sit exercitationem minus fugit, ipsam facilis quasi repellat
+          error, beatae asperiores. Expedita quod mollitia voluptatum libero
+          voluptate. Delectus, dolorem.
+        </p>
+      </section>
+      <section>
+        <h2>A quién está dirigido</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi et
+          dolores sit exercitationem minus fugit, ipsam facilis quasi repellat
+          error, beatae asperiores. Expedita quod mollitia voluptatum libero
+          voluptate. Delectus, dolorem.
+        </p>
+      </section>
+      <section>
+        <h2>Qué tecnologías o enfoques se usaron </h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi et
+          dolores sit exercitationem minus fugit, ipsam facilis quasi repellat
+          error, beatae asperiores. Expedita quod mollitia voluptatum libero
+          voluptate. Delectus, dolorem.
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export { SobreNosotros };
