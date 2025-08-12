9691d6ed38f62ccd690ec9f74b472f26373c5906
lucg-gith <garrelucia@gmail.com>
2025-08-12 20:00:37 +0100

    global styles & change image

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterApp } from "./router/RouterApp";
 import { UserProvider } from "./context/UserContext";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../src/styles/global.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
